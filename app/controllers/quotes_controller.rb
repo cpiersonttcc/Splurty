7e9a89a42091e0cab6131fc0808d43656c368b63
cpiersonttcc <cpierson@trusted-name.com>
2015-11-10 20:58:58 -0500

    correcting some db issues

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,12 +1,12 @@
 class QuotesController < ApplicationController
 	def index
-			@quote = Quote.order("RANDOM()").first
+		@quote = Quote.order("RANDOM()").first
 	end
 
 	def create
 		@quote = Quote.create(quote_params)
 		if @quote.invalid?
-			flash[:error] = '<strong>Could note save</strong> the data you entered is invalid.'
+			flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
 		end
 		redirect_to root_path
 	end
