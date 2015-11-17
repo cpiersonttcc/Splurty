530a7528e375a57ad1b574142ce88a3aba04cdf4
cpiersonttcc <cpierson@trusted-name.com>
2015-11-16 21:18:30 -0500

    quote  author initials test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-	test "the truth" do
-		assert true
+	test "unique_tag" do
+		quote = Quote.create(:author => 'Darth Vader', :saying => 'Luke, I am your father')
+		puts quote.inspect
+		expected = 'DV#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
 	end
 end
